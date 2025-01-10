4b140add55b03a17e6fc64e9884da0b9b8d7227f
realrnvr <realrnvr@gmail.com>
2025-01-11 01:26:50 +0530

    format

--- Backend/models/userprofile.js
+++ Backend/models/userprofile.js
@@ -1,49 +1,45 @@
 // const { date, array } = require("joi");
-const mongoose=require("mongoose")
-const Schema=mongoose.Schema;
+const mongoose = require("mongoose");
 
-const Userprofile=new Schema({
+const Userprofile = new mongoose.Schema({
+  username: {
+    type: String,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  place: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  education: {
+    type: String,
+  },
+  workexperices: {
+    type: String,
+  },
+  extracirrucular: {
+    type: String,
+  },
+  academics: {
+    type: String,
+  },
+  skills: {
+    type: Array,
+  },
+  currentlyworking: {
+    type: String,
+  },
+  DOB: {
+    type: Date,
+  },
+});
 
-    username:{
-        type: String,
-    },
-    name:{
-        type:String,
-        required:true,
-    },
-    place:{
-        type:String,
-    },
-    email:{
-        type:String,
-    },
-    phone:{
-        type:String,
-    },
-    education:{
-        type:String,
-    },
-    workexperices:{
-        type:String,
-    },
-    extracirrucular:{
-        type:String,
-    },
-    academics:{
-        type:String,
-    },
-    skills:{
-        type:Array,
-    },
-    currentlyworking:{
-        type:String,
-    },
-    DOB:{
-        type: Date
-    }
-    
-})
-
-
-const UserProfile=mongoose.model('userProfile',Userprofile);
-module.exports =  UserProfile ;
+const UserProfile = mongoose.model("userProfile", Userprofile);
+module.exports = UserProfile;
