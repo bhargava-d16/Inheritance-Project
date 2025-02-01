0791f43bd1b1b236f882616a85fc1b11b826e4ab
Kalp Mehta <161198830+kalpm1110@users.noreply.github.com>
2025-02-02 01:30:26 +0530

    Update Profileform.jsx

--- Frontend/src/components/userProfile/Profileform.jsx
+++ Frontend/src/components/userProfile/Profileform.jsx
@@ -226,7 +226,7 @@ const UserProfile = () => {
             ) : (
               <button
                 onClick={() => setIsEditing(true)}
-                className= {x==="Candidate"?`disabled`:`px-4 py-2 text-sm text-white bg-[#2557a7] rounded-md hover:bg-[#1e4c9a] transition-colors duration-200`}
+                className= {x==="Company"?`disabled`:`px-4 py-2 text-sm text-white bg-[#2557a7] rounded-md hover:bg-[#1e4c9a] transition-colors duration-200`}
               >
                 Edit Profile
               </button>
