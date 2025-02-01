dcd04e437bc0b7b44876eb5430493285dec6aa55
Kalp Mehta <161198830+kalpm1110@users.noreply.github.com>
2025-02-01 11:34:55 +0530

    Update Layout.jsx

--- Frontend/src/components/Layout/Layout.jsx
+++ Frontend/src/components/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../others/Header'
+import Header from '../Others/Header'
 import { Outlet } from 'react-router-dom'
 import Footer from '../Others/Footer'
 
